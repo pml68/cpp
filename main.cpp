a48eb32ff11e78435993911d1aa78a434a0f1049
pml68 <tutorialmester@gmail.com>
2024-01-07 21:06:54 +0100

    feat: optimize the code

--- main.cpp
+++ main.cpp
@@ -85,38 +85,32 @@ bool checkWinner(char *spaces, char player, char computer) {
 
   if (spaces[0] != ' ' && spaces[0] == spaces[1] && spaces[1] == spaces[2]) {
     spaces[0] == player ? std::cout << "YOU WIN\n" : std::cout << "YOU LOSE\n";
-    return true;
   } else if (spaces[3] != ' ' && spaces[3] == spaces[4] &&
              spaces[4] == spaces[5]) {
     spaces[3] == player ? std::cout << "YOU WIN\n" : std::cout << "YOU LOSE\n";
-    return true;
   } else if (spaces[6] != ' ' && spaces[6] == spaces[7] &&
              spaces[7] == spaces[8]) {
     spaces[6] == player ? std::cout << "YOU WIN\n" : std::cout << "YOU LOSE\n";
-    return true;
   } else if (spaces[0] != ' ' && spaces[0] == spaces[3] &&
              spaces[3] == spaces[6]) {
     spaces[0] == player ? std::cout << "YOU WIN\n" : std::cout << "YOU LOSE\n";
-    return true;
   } else if (spaces[1] != ' ' && spaces[1] == spaces[4] &&
              spaces[4] == spaces[7]) {
     spaces[1] == player ? std::cout << "YOU WIN\n" : std::cout << "YOU LOSE\n";
-    return true;
   } else if (spaces[2] != ' ' && spaces[2] == spaces[5] &&
              spaces[5] == spaces[8]) {
     spaces[2] == player ? std::cout << "YOU WIN\n" : std::cout << "YOU LOSE\n";
-    return true;
   } else if (spaces[0] != ' ' && spaces[0] == spaces[4] &&
              spaces[4] == spaces[8]) {
     spaces[0] == player ? std::cout << "YOU WIN\n" : std::cout << "YOU LOSE\n";
-    return true;
   } else if (spaces[2] != ' ' && spaces[2] == spaces[4] &&
              spaces[4] == spaces[6]) {
     spaces[2] == player ? std::cout << "YOU WIN\n" : std::cout << "YOU LOSE\n";
-    return true;
+  } else {
+    return false;
   }
 
-  return false;
+  return true;
 }
 
 bool checkTie(char *spaces) { return true; }
